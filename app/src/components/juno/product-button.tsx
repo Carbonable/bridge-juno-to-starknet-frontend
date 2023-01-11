8b607a7405f61dcad65643ffbf6c6062b0062411
Valentin Dosimont <valentin.dosimont@gmail.com>
2023-01-11 18:31:51 +0100

    fix: ui improvements

--- app/src/components/juno/product-button.tsx
+++ app/src/components/juno/product-button.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import cx from "classnames";
 import { Button } from "~/src/components/button";
 import { useMigrateTokens } from "~/src/hook/useMigrateTokens";
@@ -14,18 +14,23 @@ export function ProductButton({
   projectAddress,
   starknetProjectAddress,
 }: ProductButtonProps) {
-  const { handleBurnTokens, handleMigrateTokens, hasBurn } = useMigrateTokens({
+  const { handleBurnTokens, handleMigrateTokens } = useMigrateTokens({
     tokens,
     projectAddress,
     starknetProjectAddress,
   });
+  const [hasBurn, setHasBurn] = useState(false);
   const hasTokens = undefined !== tokens && 0 < tokens.length;
+  useEffect(() => {
+    const storedValue = localStorage.getItem(`hasBurn-${projectAddress}`);
+    setHasBurn(null !== storedValue);
+  }, [hasBurn, projectAddress]);
   return (
     <div>
       <Button onClick={handleBurnTokens} canHover={hasTokens}>
         Burn
       </Button>
-      <Button onClick={handleMigrateTokens} canHover={hasTokens}>
+      <Button onClick={handleMigrateTokens} canHover={hasBurn && hasTokens}>
         Migrate
       </Button>
     </div>
